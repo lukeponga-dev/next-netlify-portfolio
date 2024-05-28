5c0f20ce9d36a2216aea180ab1231cd03215f2b4
Luke Ponga <51413507+lukeponga-dev@users.noreply.github.com>
2024-05-28 17:48:44 +1200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 // import Header from '@components/Header'
 // import Footer from '@components/Footer'
-import '@styles/global.css';
 
 
 export default function Home() {
