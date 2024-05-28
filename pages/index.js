6602857c919859d19454bc76cc3aae9c0d730377
Luke Ponga <51413507+lukeponga-dev@users.noreply.github.com>
2024-05-28 17:36:58 +1200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 // import Header from '@components/Header'
 // import Footer from '@components/Footer'
-
+import '../styles/style.css';
 export default function Home() {
   return (
         <div className="portfolio">
