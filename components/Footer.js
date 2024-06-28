98e7a0dd8d0836d57c1ca59c0a5a9bc537cb8bc3
Luke Ponga <51413507+lukeponga-dev@users.noreply.github.com>
2024-06-29 11:26:49 +1200

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import '@components/Footer';
+import styles from './Footer.module.css'; // Corrected import statement
 
 const Footer = () => {
   return (
-      <footer>
+      <footer className={styles.footer}>
           <p>© 2024 Lukeponga-dev</p>
       </footer>
   );
 }
+
 export default Footer;
