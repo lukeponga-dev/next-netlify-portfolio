31353afeb1cef18702d111267c4ac53ba7b6b262
Luke Ponga <51413507+lukeponga-dev@users.noreply.github.com>
2025-03-14 00:45:00 +1300

    Update AboutMe.jsx

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -10,20 +10,37 @@ function AboutMe() {
             <p>When I'm not coding, you can find me exploring the great outdoors, camera in hand. I'm an avid hiker and photographer, and I often combine these hobbies to capture the beauty of nature. These adventures reinforce my belief in continuous learning and improvement, principles I carry into my professional life.</p>
             <p>I'm excited about the future of technology and am eager to collaborate with experienced developers and continue learning in this dynamic environment. If you're interested in working together or just want to say hi, feel free to reach out at lukeponga9@gmail.com.</p>
 
-            <h2>Skills</h2>
+ <h2>Skills</h2>
+<div>
+    <h3>Programming Languages</h3>
+    <ul>
+        <li>C#</li>
+        <li>JavaScript</li>
+    </ul>
+</div>
+<div>
+    <h3>Frameworks/Libraries</h3>
+    <ul>
+        <li>.NET Platform</li>
+        <li>ASP.NET</li>
+        <li>React</li>
+    </ul>
+</div>
+<div>
+    <h3>Databases</h3>
+    <ul>
+        <li>MS SQL Server</li>
+        <li>PostgreSQL</li>
+    </ul>
+</div>
+<div>
+    <h3>Tools</h3>
+    <ul>
+        <li>Git</li>
+        <li>Agile Methodologies</li>
+    </ul>
+</div>
 
-            <ul>
-                <li>.NET platform (C#)</li>
-                <li>Strong knowledge of JavaScript, HTML, and CSS</li>
-                <li>Problem-solving and logical reasoning abilities</li>
-                <li>Experience with database management (MS SQL Server, PostgreSQL)</li>
-                <li>Detail-oriented with excellent time management skills</li>
-                <li>Agile methodologies</li>
-                <li>API development skills</li>
-                <li>Ability to work effectively in a team environment</li>
-                <li>Eagerness to learn and adapt to new technologies</li>
-                <li>Creative thinking and innovative approach to software development</li>
-            </ul>
         </div>
     );
 }
